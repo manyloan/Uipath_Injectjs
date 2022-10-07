1bb5480b93b1340caf591fd6e516f9161108f94d
Haniel Loan <haniel.loan@gmail.com>
2022-10-07 20:43:27 -0300

    Add files via upload

---
+++ Files/inject.js
@@ -0,0 +1,13 @@
+function(element){
+    document.getElementById("customerName").value = "COMPANYNAME";
+    document.getElementById("customerID").value = "CUSTOMERID";
+    document.getElementById("primaryContact").value = "PRIMARYCONTACT";
+    document.getElementById("street").value = "STREETADDRESS";
+    document.getElementById("city").value = "CITY";
+    document.getElementById("state").value = "STATE";
+    document.getElementById("zip").value = "ZIP";
+    document.getElementById("email").value = "EMAILADDRESS";
+    document.getElementById("VARIAVELDESCONTO").checked = true;
+    NONDISCLOSURE
+    SUBMITCLICK
+}
